1dfc4bc02a4019578410f6d6e789839491ef9548
Jacob Henderson <jacobth495@gmail.com>
2022-09-15 12:30:17 -0500

    Added event to check inputs with each button click and fixed bug that would let you add more than two inputs

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ const subtractBtn = document.querySelector('#subtract');
 const clearBtn = document.querySelector('#clear');
 const backspaceBtn = document.querySelector('#backspace');
 const equalBtn = document.querySelector('#equal');
+const buttons = document.querySelectorAll('button');
+const operatorBtns = document.querySelectorAll('.func')
 
 function add(a, b) {
 	c = a + b;
@@ -78,55 +80,67 @@ zeroBtn.addEventListener('click', function() {
 })
 
 multiplyBtn.addEventListener('click', function() {
-  inputOne = parseInt(screen.textContent);
   screen.textContent += ' * ';
 })
 
 divideBtn.addEventListener('click', function() {
-  inputOne = parseInt(screen.textContent);
   screen.textContent += ' / ';
 })
 
 addBtn.addEventListener('click', function() {
-  inputOne = parseInt(screen.textContent);
   screen.textContent += ' + ';
 })
 
 subtractBtn.addEventListener('click', function() {
-  inputOne = parseInt(screen.textContent);;
   screen.textContent += ' - ';
 })
 
-equalBtn.addEventListener('click', function() {
-  problem = screen.textContent.split(' ');
-  inputTwo = parseInt(problem[2]);
-  if (problem[1] === '/' && inputTwo === 0) {
-    alert("You can't divide by 0 silly!!!");
-  }
-  switch (problem[1]) {
-    case '*':
-      screen.textContent = inputOne + ' * ' + inputTwo +
-      ' = ' + multiply(inputOne, inputTwo);
-      break;
-    case '/':
-      screen.textContent = inputOne + ' / ' + inputTwo +
-      ' = ' + divide(inputOne, inputTwo);
-      break;
-    case '-':
-      screen.textContent = inputOne + ' - ' + inputTwo +
-      ' = ' + subtract(inputOne, inputTwo);
-      break;
-    case '+':
-      screen.textContent = inputOne + ' + ' + inputTwo +
-      ' = ' + add(inputOne, inputTwo);
-      break;
-  }
-})
+equalBtn.addEventListener('click', operate)
 
 clearBtn.addEventListener('click', function() {
   screen.textContent = '';
+  inputs = ''
 })
 
 backspaceBtn.addEventListener('click', function() {
   screen.textContent = screen.textContent.slice(0, -1);
 })
+
+function checkInputs() {
+  inputs = screen.textContent.split(' ');
+  inputOne = parseInt(inputs[0]);
+  operator = inputs[1];
+  inputTwo = parseInt(inputs[2]);
+}
+
+for (b of buttons) {
+  b.addEventListener('click', checkInputs);
+}
+
+function operate() {
+  if (operator === '/' && inputTwo === 0) {
+    alert("You can't divide by 0 silly!!!");
+  }
+  switch (operator) {
+    case '*':
+      screen.textContent = multiply(inputOne, inputTwo);
+      break;
+    case '/':
+      screen.textContent = divide(inputOne, inputTwo);
+      break;
+    case '-':
+      screen.textContent = subtract(inputOne, inputTwo);
+      break;
+    case '+':
+      screen.textContent = add(inputOne, inputTwo);
+      break;
+  }
+}
+
+for (o of operatorBtns) {
+  o.addEventListener('click', function checkText() {
+    if (inputs.length >= 4) {
+      operate();
+    }
+  })
+}
